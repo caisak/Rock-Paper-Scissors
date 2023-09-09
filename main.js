5d54d6d8292001ff453affd763081cfa6d11f684
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-09-09 09:45:35 +0200

    swedish and styling

--- main.js
+++ main.js
@@ -1,56 +1,37 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => { 
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
-
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+}));
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1
-    
-    if (randomNumber === 1) {
-        computerChoice = 'rock'
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'scissors'
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'paper'
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
-
+    const choices = ['sten', 'sax', 'påse'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    computerChoice = choices[randomNumber];
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
-    if (computerChoice === userChoice) {
-        result = 'its a draw!'
+    if (computerChoice.toLowerCase() === userChoice.toLowerCase()) {
+        result = 'Det blev oavgjort!';
+    } else {
+        const winningCombination = {
+            sten: 'sax',
+            påse: 'sten',
+            sax: 'påse'
+        };
+
+        result = winningCombination[userChoice.toLowerCase()] === computerChoice.toLowerCase() ? 'Wiii du vann! :D' : 'Du förlorade :(';
     }
-    if (computerChoice === 'rock' && userChoice === "paper") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'rock' && userChoice === "scissors") {
-        result = 'you lose!'
-    }
-    if (computerChoice === 'paper' && userChoice === "scissors") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'paper' && userChoice === "rock") {
-        result = 'you lose!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "rock") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "paper") {
-        result = 'you lose!'
-    }
-    resultDisplay.innerHTML = result
+
+    resultDisplay.innerHTML = result;
 }
